cd8726446463e40f3c3a27f45430907dba1f981c
Egba samuel Chialuka <burgeressence.eth@gmail.com>
2025-08-11 02:12:44 +0100

    changes made

--- src/components/MSLBackground.tsx
+++ src/components/MSLBackground.tsx
@@ -1,9 +1,23 @@
 
 import React from 'react';
 
-const MSLBackground = () => {
+type MSLBackgroundProps = {
+  opacity?: number; // 0..1, controls visibility intensity
+  shadow?: number; // 0..1, controls glow intensity
+  className?: string;
+};
+
+const MSLBackground: React.FC<MSLBackgroundProps> = ({ opacity, shadow, className }) => {
+  const style: React.CSSProperties = {};
+  if (opacity !== undefined) {
+    (style as any)['--msl-opacity'] = opacity;
+  }
+  if (shadow !== undefined) {
+    (style as any)['--msl-shadow'] = shadow;
+  }
+
   return (
-    <div className="msl-text-bg">
+    <div className={`msl-text-bg ${className ?? ''}`} style={style}>
       <div className="msl-text">MSL</div>
       <div className="msl-text">MSL</div>
       <div className="msl-text">MSL</div>
